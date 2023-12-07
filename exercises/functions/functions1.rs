417ea935e70a57981ee588676ee2f4fe119850fc
Alan-MQ <maqing@dongyu-technology.com>
2023-12-07 18:50:47 +0800

    practice

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -3,8 +3,10 @@
 // Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
+    fn call_me() -> i32 {
+        0
+    }
     call_me();
 }
